949378ed134466ffedf2d5820bd54698a3ea7ad3
Christian Fleschhut <weltrekordhalter@gmail.com>
2016-06-09 14:13:51 +0200

    display score and game ending text

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,21 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+	public Text countText;
+	public Text winText;
 
 	private Rigidbody rb;
+	private int count;
 
 	void Start () {
 		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
 	}
 
 	void FixedUpdate () {
@@ -23,6 +30,15 @@ public class PlayerController : MonoBehaviour {
 	void OnTriggerEnter(Collider other) {
 		if (other.gameObject.CompareTag("Pick Up")) {
 			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText () {
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 8) {
+			winText.text = "You win!";
 		}
 	}
 }
